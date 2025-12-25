2105b086b4df4ee9adce64782d33801d6493b95e
sudecevv <ecesudevural@gmail.com>
2025-12-25 22:34:53 +0300

    Proje tamamlandı.

--- config/config.js
+++ config/config.js
@@ -1,10 +1,7 @@
 // config/config.js
 module.exports = {
-  host: 'localhost',
-  user: 'root',
-  password: '',
-  database: 'sunglasshot_kds'
-
-  
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME
 };
-
